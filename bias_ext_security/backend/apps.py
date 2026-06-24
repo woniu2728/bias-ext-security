6eb4d578c5283a6ee1ccf07d94650459db489920
woniu <2463396298@qq.com>
2026-06-24 12:14:36 +0800

    Add apps.py + django_migrations

---
+++ bias_ext_security/backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class SecurityExtensionConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bias_ext_security.backend"
+    label = "security"
